9dd103bcfa6c21039e5e298dffedf4196450a98a
SatoshiOhashi1998 <nagaomasaki.lauren@gmail.com>
2026-09-05 10:22:54 +0900

    feat: youtubeのページ遷移を監視

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,20 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("My YouTube Comments installed");
 });
 
+
 chrome.sidePanel
     .setPanelBehavior({
         openPanelOnActionClick: true
     })
     .catch(console.error);
+
+
+// YouTubeのページが更新されたとき
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+        chrome.runtime.sendMessage({
+            type: "youtube-url-changed",
+            url: changeInfo.url
+        });
+    }
+});
