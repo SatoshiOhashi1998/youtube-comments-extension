64c7f7e26ca2f49127aa518c8e3d834b66eadd84
SatoshiOhashi1998 <nagaomasaki.lauren@gmail.com>
2026-09-05 10:26:58 +0900

    feat: YouTubeのページ遷移を監視

--- sidepanel.js
+++ sidepanel.js
@@ -8,8 +8,13 @@ async function getCurrentVideoId() {
         return null;
     }
 
+    return getVideoIdFromUrl(tab.url);
+}
+
+
+function getVideoIdFromUrl(urlString) {
     try {
-        const url = new URL(tab.url);
+        const url = new URL(urlString);
 
         if (url.hostname !== "www.youtube.com") {
             return null;
@@ -24,10 +29,8 @@ async function getCurrentVideoId() {
 }
 
 
-async function updateVideoId() {
+function displayVideoId(videoId) {
     const videoIdElement = document.getElementById("video-id");
-
-    const videoId = await getCurrentVideoId();
 
     if (videoId) {
         videoIdElement.textContent = videoId;
@@ -37,4 +40,20 @@ async function updateVideoId() {
 }
 
 
+async function updateVideoId() {
+    const videoId = await getCurrentVideoId();
+    displayVideoId(videoId);
+}
+
+
+// 最初にSide Panelを開いたとき
 updateVideoId();
+
+
+// YouTubeのURL変更通知を受け取る
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.type === "youtube-url-changed") {
+        const videoId = getVideoIdFromUrl(message.url);
+        displayVideoId(videoId);
+    }
+});
